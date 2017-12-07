bee0d4a4c75947da11fafe7731d67b5c3edb46dc
a.roshanzamir1996 <a.roshanzamir1996@outlook.com>
2017-12-07 17:01:14 +0330

    Removing some extra dependencies.
    Related Work Items: #123

--- BSN.Commons/BSN.Commons/Users/Roles.cs
+++ BSN.Commons/BSN.Commons/Users/Roles.cs
@@ -1,16 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Commons.Users
+﻿namespace Commons.Users
 {
 	public enum Roles
 	{
-		[Display(Name = "مهمان")]
 		Guest,
-		[Display(Name = "مدیر سیستم")]
 		Admin,
-		[Display(Name = "دکتر")]
 		Doctor,
-		[Display(Name = "منشی")]
 		Secretary
 	}
 }
