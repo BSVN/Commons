c7211193da8ad46d94f9420fc5c4d207c68b34b3
mohammad reza alizadeh <m.reza.alizadeh1370@hotmail.com>
2019-11-25 11:49:16 +0330

    Add Queue to unit of work

--- Source/BSN.Commons/Infrastructure/IUnitOfWork.cs
+++ Source/BSN.Commons/Infrastructure/IUnitOfWork.cs
@@ -1,7 +1,11 @@
-﻿namespace Commons.Infrastructure
+﻿using System;
+
+namespace Commons.Infrastructure
 {
     public interface IUnitOfWork
     {
         void Commit();
+
+        void AddToQueue(Func<bool> func);
     }
 }
--- Source/BSN.Commons/Infrastructure/UnitOfWork.cs
+++ Source/BSN.Commons/Infrastructure/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Commons.Infrastructure
 {
@@ -6,20 +8,32 @@ namespace Commons.Infrastructure
 	{
 		private readonly IDatabaseFactory _databaseFactory;
 		private DbContext _dataContext;
+        private Queue<Func<bool>> Queue { get; set; }
 
 
-		protected DbContext DataContext => _dataContext ?? (_dataContext = _databaseFactory.Get());
+        protected DbContext DataContext => _dataContext ?? (_dataContext = _databaseFactory.Get());
 
 
 		public UnitOfWork(IDatabaseFactory databaseFactory)
 		{
 			_databaseFactory = databaseFactory;
+            Queue = new Queue<Func<bool>>();
 		}
 
+        public void AddToQueue(Func<bool> func)
+        {
+            Queue.Enqueue(func);
+        }
 
-		public void Commit()
+        public void Commit()
 		{
 			DataContext.SaveChanges();
-		}
+
+            while (Queue.Count > 0)
+            {
+                var Function = Queue.Dequeue();
+                Function.Invoke();
+            }
+        }
 	}
 }
