d996917be43094e1d910e857cf916e5b31b9091a
RezaNoei <RezaNoei@DESKTOP-L1JJH35>
2020-10-21 13:43:37 +0330

    Update IDbContext And Response Base To have a better Implementation

--- Source/BSN.Commons.PresentationInfrastructure/ResponseBase.cs
+++ Source/BSN.Commons.PresentationInfrastructure/ResponseBase.cs
@@ -6,7 +6,10 @@ namespace BSN.Commons.PresentationInfrastructure
 {
     public abstract class ResponseBase : IResponse<ValidationResult>
     {
-        public virtual bool IsSuccess => StatusCode == ResponseStatusCode.OK;
+        public virtual bool IsSuccess => StatusCode == ResponseStatusCode.OK 
+                                         || StatusCode == ResponseStatusCode.Accepted 
+                                         || StatusCode == ResponseStatusCode.Created 
+                                         || StatusCode == ResponseStatusCode.PartialContent;
 
         public string Message { get; set; }
 
