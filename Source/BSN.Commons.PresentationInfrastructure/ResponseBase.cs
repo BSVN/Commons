10b9aff5887faaa42c9aa2fd6444757db382ae3c
RezaNoei <RezaNoei@DESKTOP-L1JJH35>
2020-12-06 13:03:45 +0330

    Update ResponseBase to AutoDetect SuccessStatusCodes.

--- Source/BSN.Commons.PresentationInfrastructure/ResponseBase.cs
+++ Source/BSN.Commons.PresentationInfrastructure/ResponseBase.cs
@@ -6,10 +6,14 @@ namespace BSN.Commons.PresentationInfrastructure
 {
     public abstract class ResponseBase : IResponse<ValidationResult>
     {
-        public virtual bool IsSuccess => StatusCode == ResponseStatusCode.OK 
-                                         || StatusCode == ResponseStatusCode.Accepted 
-                                         || StatusCode == ResponseStatusCode.Created 
-                                         || StatusCode == ResponseStatusCode.PartialContent;
+        /// <summary>
+        /// Gets a value that indicates whether the HTTP response was successful.
+        /// </summary>
+        /// <remarks>
+        /// If the server doesn't return a successful HttpStatusCode in the Successful range (200-299) for the request, then the responseObject.IsSuccess property is set to false
+        /// </remarks>
+        /// More info: https://docs.microsoft.com/en-us/uwp/api/windows.web.http.httpresponsemessage.issuccessstatuscode?view=winrt-19041
+        public virtual bool IsSuccess => (int)StatusCode >= 200 && (int)StatusCode <= 299;
 
         public string Message { get; set; }
 
