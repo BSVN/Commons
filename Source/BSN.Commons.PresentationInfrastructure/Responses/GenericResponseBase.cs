3dcb0b136e940fd2ca1a9ff2be20d5467845ddb0
R.Noei <reza.noei@chmail.ir>
2023-06-08 18:27:34 +0330

    Revert old response types.

---
+++ Source/BSN.Commons.PresentationInfrastructure/Responses/GenericResponseBase.cs
@@ -0,0 +1,11 @@
+﻿namespace BSN.Commons.Responses
+{
+    /// <summary>
+    /// Generic base response. 
+    /// </summary>
+    /// <typeparam name="T"> The type of object or value handled by the class. </typeparam>
+    public class GenericResponseBase<T> : Response where T : class
+    {
+        public T Data { get; set; }
+    }
+}
---
+++ Source/BSN.Commons.PresentationInfrastructure/Responses/GenericResponseBaseWithPagination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BSN.Commons.Responses
+{
+    /// <summary>
+    /// Generic response base for paginated data.
+    /// </summary>
+    /// <remarks>
+    /// Paginated response provides metadata for navigation purpose.
+    /// </remarks>
+    /// <typeparam name="T">Data type.</typeparam>
+    [Obsolete("Due to incompatability with Grpc this response type is only used for backward compatibility.")]
+    public class GenericResponseBaseWithPagination<T> : GenericResponseBase<T> where T : class
+    {
+        /// <summary>
+        /// Pagination metada used by the client as the parameters for navigation through whole records.
+        /// </summary>
+        public PaginationMetadata Meta { get; set; }
+    }
+}
