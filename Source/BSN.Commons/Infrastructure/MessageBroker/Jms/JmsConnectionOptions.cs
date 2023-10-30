b9515a55386576e6a8daa67c19d85f27ee445213
meghasemim1999 <30366887+meghasemim1999@users.noreply.github.com>
2023-10-30 08:00:27 +0330

    Add JmsEventAggregator (#50)

---
+++ Source/BSN.Commons/Infrastructure/MessageBroker/Jms/JmsConnectionOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace BSN.Commons.Infrastructure.MessageBroker.Jms
+{
+    /// <summary>
+    /// Represents a class containing options for configuring a JMS connection.
+    /// </summary>
+    public class JmsConnectionOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JmsConnectionOptions"/> class.
+        /// </summary>
+        /// <param name="brokerUri"></param>
+        /// <param name="queueName"></param>
+        public JmsConnectionOptions(string brokerUri, string queueName)
+        {
+            BrokerUri = brokerUri;
+            QueueName = queueName;
+        }
+
+        /// <summary>
+        /// For example: tcp://localhost:61616
+        /// </summary>
+        public string BrokerUri { get; }
+        
+        /// <summary>
+        /// For example: testQueue
+        /// </summary>
+        public string QueueName { get; }
+    }
+}
