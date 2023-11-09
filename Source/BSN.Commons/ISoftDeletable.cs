672f6b21bb8525f8005cb896272eaac164872b7e
soroshsabz <soorosh_abi@hotmail.com>
2023-11-09 12:47:22 +0330

    Add soft deletable interface

---
+++ Source/BSN.Commons/ISoftDeletable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BSN.Commons
+{
+    /// <summary>
+    /// Provides a mechanism for soft deletion.
+    ///
+    /// Soft deletion means that this object is not completely destroyed, but it goes in a deleted state,
+    /// and you can ask this object if you are deleted or not?
+    /// </summary>
+    public interface ISoftDeletable
+    {
+        /// <summary>
+        /// Shows the time of deletion.
+        /// </summary>
+        /// <remarks>
+        /// If this object is not deleted, this property returns <see cref="DateTime.MinValue"/>.
+        /// </remarks>
+        DateTime DeletedAt
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Delete this object.
+        /// </summary>
+        void Delete();
+    }
+}
