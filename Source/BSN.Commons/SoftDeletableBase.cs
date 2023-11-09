4d4c7ead6dfd0ef5a5751bd7c518ca00a148e185
soroshsabz <soorosh_abi@hotmail.com>
2023-11-09 13:12:15 +0330

    Add base class.

---
+++ Source/BSN.Commons/SoftDeletableBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSN.Commons
+{
+    /// <inheritdoc/>
+    public abstract class SoftDeletableBase : ISoftDeletable
+    {
+        /// <inheritdoc/>
+        public DateTime DeletedAt
+        {
+            get;
+            private set;
+        }
+
+        /// <inheritdoc/>
+        public virtual void Delete()
+        {
+            if (DeletedAt != DateTime.MinValue)
+            {
+                throw new InvalidOperationException("This object is already deleted.");
+            }
+
+            DeletedAt = DateTime.UtcNow;
+        }
+    }
+}
