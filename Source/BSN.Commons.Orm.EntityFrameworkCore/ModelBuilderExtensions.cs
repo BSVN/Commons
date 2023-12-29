b327d94e6f52db3b15153eee244c113a72358229
soroshsabz <soorosh_abi@hotmail.com>
2023-12-29 13:25:10 +0330

    Update docs.

--- Source/BSN.Commons.Orm.EntityFrameworkCore/ModelBuilderExtensions.cs
+++ Source/BSN.Commons.Orm.EntityFrameworkCore/ModelBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
@@ -14,8 +15,20 @@ namespace BSN.Commons.Orm.EntityFrameworkCore
     public static class ModelBuilderExtensions
     {
         /// <summary>
-        /// Map all readonly property of entity to model
+        /// Entity Framework Core does not support mapping get-only properties to columns by default.
+        /// So if you want to map get-only properties to columns, you need to use this extension method.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// modelBuilder.Entity{Foo}().MapAllReadonlyProperty();
+        /// modelBuilder.Entity{Foo}().Ignore(F => F.SomeGetOnlyProperty); // Ignore get-only property
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// After calling this method, you can use
+        /// <see cref="EntityTypeBuilder{TEntity}.Ignore(Expression{Func{TEntity, object?}})"/> method,
+        /// to ignore some get-only properties.
+        /// </remarks>
         /// <typeparam name="T">The entity type being configured.</typeparam>
         /// <param name="builder">The builder for the entity type being configured.</param>
         public static void MapAllReadonlyProperty<T>(this EntityTypeBuilder<T> builder) where T : class
