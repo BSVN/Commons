f34e4f16abf4c87bd868d0e1486b39ba638b9b75
Reza Noei <Reza.Noei@chmail.ir>
2024-05-06 14:29:57 +0330

    Initial Commit.

---
+++ Source/BSN.Commons.AutoMapper/CommonsMappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BSN.Commons.Responses;
+
+namespace BSN.Commons.AutoMapper
+{
+    public class CommonMapperProfile : Profile
+    {
+        public CommonMapperProfile()
+        {
+            CreateMap(typeof(PagedEntityCollection<>), typeof(PaginationMetadata)).ConvertUsing(typeof(PagedEntityCollectionToMetaDataConverter<>));
+
+            CreateMap(typeof(IEnumerable<>), typeof(CollectionViewModel<>)).ConvertUsing(typeof(GenericIEnumerableToCollectionViewModelConverter<,>));
+        }
+
+        private class PagedEntityCollectionToMetaDataConverter<TDomain> : ITypeConverter<PagedEntityCollection<TDomain>, PaginationMetadata>
+        {
+            public PaginationMetadata Convert(PagedEntityCollection<TDomain> source, PaginationMetadata destination, ResolutionContext context)
+            {
+                return new PaginationMetadata()
+                {
+                    Page = source.CurrentPage,
+                    PageCount = source.PageSize,
+                    PageSize = source.PageSize,
+                    RecordCount = source.RecordCount
+                };
+            }
+        }
+
+        private class GenericIEnumerableToCollectionViewModelConverter<TDomain, TViewModel> : ITypeConverter<IEnumerable<TDomain>, CollectionViewModel<TViewModel>>
+        {
+            public CollectionViewModel<TViewModel> Convert(IEnumerable<TDomain> source, CollectionViewModel<TViewModel> destination, ResolutionContext context)
+            {
+                return new CollectionViewModel<TViewModel>
+                {
+                    Items = context.Mapper.Map<IEnumerable<TViewModel>>(source)
+                };
+            }
+        }
+    }
+}
---
+++ Source/BSN.Commons.AutoMapper/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BSN.Commons.AutoMapper.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services, Action<IMapperConfigurationExpression> configure)
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                MapperConfigurationExpression mapperConfigurationExpression = new MapperConfigurationExpression();
+                configure(mapperConfigurationExpression);
+
+                configure(config);
+
+                config.AddProfile(new CommonMapperProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
+            
+            return services;
+        }
+    }
+}
