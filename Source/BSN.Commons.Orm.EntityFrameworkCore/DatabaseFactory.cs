b5c1ba5d1ac45af6daa65511fe3a563f22c9c634
m.rahimian <m.rahimian@ressa.net>
2026-09-01 13:05:18 +0330

    add DatabaseFactory default implimentation for ef core

---
+++ Source/BSN.Commons.Orm.EntityFrameworkCore/DatabaseFactory.cs
@@ -0,0 +1,29 @@
+﻿using BSN.Commons.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSN.Commons.Orm.EntityFramework
+{
+    public class DatabaseFactory<TContext> : Disposable, IDatabaseFactory where TContext : DbContext, IDbContext, IAsyncDbContext
+    {
+        public DatabaseFactory(TContext context)
+        {
+            _dataContext = context;
+        }
+
+        public IDbContext Get()
+        {
+            return _dataContext;
+        }
+
+        public void Dispose()
+        {
+            _dataContext?.Dispose();
+        }
+
+        public IAsyncDbContext GetAsyncContext()
+        {
+            return _dataContext;
+        }
+        protected TContext _dataContext;
+    }
+}
